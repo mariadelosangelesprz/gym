d3488c3eda445bc84c2d26cad43210684dcb7b64
maria delos angeles pérez <mariadelosangelesprz@gmail.com>
2022-10-26 20:33:37 -0300

    reformulacion de formulario validar

--- comportamiento.js
+++ comportamiento.js
@@ -10,21 +10,32 @@ function enviarFormulario(){
 
     var mensajesError = [];
     if (nombre.value === null || nombre.value===''){
-         mensajesError.push('Ingresa tu nombre');
+          alert("Tiene que escribir su nombre")
+          document.
+          document.formulario.nombre.focus()
+          return 0;
+
     }
     
     if (email.value === null || email.value===''){
-         mensajesError.push('Ingresa tu email');
+          alert("Tiene que escribir su mail")
+          document.formulario.email.focus()
+          return 0;
+
     }
     if (asunto.value === null || asunto.value===''){
-         mensajesError.push('Ingresa tu asunto');
+          alert("Tiene que escribir un asunto")
+          document.formulario.asunto.focus()
+          return 0;
+
    }
    if (mensaje.value === null || mensaje.value===''){
     mensajesError.push('Ingresa tu mensaje');
    }
+    
+    //error.innerHTML=  mensajesError.join(', ');
+    //return false;
+    alert("Muchas gracias por enviar el formulario");
+    document.formulario.submit();
 
-    error.innerHTML=  mensajesError.join(', ');
-
-
-    return false;
 }
